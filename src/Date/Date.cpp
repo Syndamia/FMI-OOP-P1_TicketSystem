9c6f8947e63738387842f02a6deaa95b84e9a7f5
Syndamia <kamen.d.mladenov@protonmail.com>
2022-04-25 17:32:55 +0300

    Autocommit at 25.04.2022 @ 17:32

---
+++ src/Date/Date.cpp
@@ -0,0 +1,7 @@
+#include "Date.h"
+
+Date::Date(unsigned day, unsigned month, unsigned year) {
+	this->day = day;
+	this->month = month;
+	this->year = year;
+}
---
+++ src/Date/Date.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_DATE
+#define HEADER_DATE
+
+class Date {
+	unsigned day;
+	unsigned month;
+	unsigned year;
+
+public:
+	Date(unsigned day, unsigned month, unsigned year);
+};
+
+#endif
