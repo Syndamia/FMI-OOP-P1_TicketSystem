5b3ff6722b03b7ddfb0c521ae7176f42e72de433
Syndamia <kamen.d.mladenov@protonmail.com>
2022-05-01 09:30:56 +0300

    Autocommit at 01.05.2022 @ 09:30

---
+++ src/ConsoleInterface/Menu.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_CONSOLEINTERFACE_MENU
+#define HEADER_CONSOLEINTERFACE_MENU
+
+#define exec (exec)(char input)
+
+class Menu {
+	char message[256];
+	void *exec;
+
+public:
+
+};
+
+#endif
