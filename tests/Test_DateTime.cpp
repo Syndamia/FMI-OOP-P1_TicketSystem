d6eb1cfde134235cd4826e4788be6de1a3d4bae2
Syndamia <kamen.d.mladenov@protonmail.com>
2022-05-01 11:09:30 +0300

    Autocommit at 01.05.2022 @ 11:09

--- tests/Test_DateTime.cpp
+++ tests/Test_DateTime.cpp
@@ -1,5 +1,18 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../doctest/doctest/doctest.h"
-
 #include "../src/DateTime/DateTime.h"
 
+TEST_SUITE_BEGIN("ClassDateTime");
+
+TEST_SUITE("Constructors") {
+	TEST_CASE("Ctor_SavesValues_WhenGivenCorrectOnes") {
+		DateTime dt(2, 49, 4, 12, 2022);
+		CHECK_EQ(dt.get_hour(), 2);
+		CHECK_EQ(dt.get_minute(), 49);
+		CHECK_EQ(dt.get_day(), 4);
+		CHECK_EQ(dt.get_month(), 12);
+		CHECK_EQ(dt.get_year(), 2022);
+	}
+}
+
+TEST_SUITE_END();
