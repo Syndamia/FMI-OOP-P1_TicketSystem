62c978e0b669a564468852922bbb83abd1eb635f
Syndamia <kamen.d.mladenov@protonmail.com>
2022-05-01 10:41:20 +0300

    Autocommit at 01.05.2022 @ 10:41

--- src/EventSystem/EventSystem.h
+++ src/EventSystem/EventSystem.h
@@ -1,8 +1,16 @@
 #ifndef HEADER_EVENTSYSTEM
 #define HEADER_EVENTSYSTEM
 
-class EventSystem {
+#include "../List/List.h"
 
+#include "Event.h"
+
+class EventSystem {
+	List<Event> events;
+	unsigned indSoonestUpcoming;
+
+public:
+	EventSystem(const Event* events, unsigned eventCount);
 };
 
 #endif
--- src/Venue.h
+++ src/Venue.h
@@ -8,6 +8,12 @@
 class Venue {
 	List<Hall> halls;
 	EventSystem es;
+
+public:
+	Venue(const Hall* halls, unsigned hallCount, const EventSystem& es);
+
+	const List<Hall> get_halls() const;
+	const EventSystem get_es() const;
 };
 
 #endif
