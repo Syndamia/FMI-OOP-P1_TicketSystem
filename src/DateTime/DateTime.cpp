bdf33676953751811f382c9dfbd73d55bcdfc6c5
Syndamia <kamen.d.mladenov@protonmail.com>
2022-05-01 08:45:39 +0300

    Autocommit at 01.05.2022 @ 08:45

--- src/DateTime/DateTime.cpp
+++ src/DateTime/DateTime.cpp
@@ -1,6 +1,8 @@
 #include "DateTime.h"
 
-DateTime::DateTime(unsigned char hour, unsigned char minute, unsigned char day, unsigned char month, unsigned short year) {
+DateTime::DateTime(unsigned short hour, unsigned short minute, unsigned short day, unsigned short month, unsigned short year) {
+	this->hour = hour;
+	this->minute = minute;
 	this->day = day;
 	this->month = month;
 	this->year = year;
@@ -9,5 +11,5 @@ DateTime::DateTime(unsigned char hour, unsigned char minute, unsigned char day, 
 int DateTime::compare(const DateTime& other) {
 	if (year < other.year || month < other.month || day < other.day || hour < other.hour || minute < other.minute)
 		return -1;
-	return year > other.year && month > other.month && day > other.day && hour > other.hour && minute > other.minute;
+	return year > other.year || month > other.month || day > other.day || hour > other.hour || minute > other.minute;
 }
--- src/DateTime/DateTime.h
+++ src/DateTime/DateTime.h
@@ -2,15 +2,26 @@
 #define HEADER_DATETIME
 
 class DateTime {
-	unsigned char hour;
-	unsigned char minute;
-	unsigned char day;
-	unsigned char month;
+	unsigned short hour;
+	unsigned short minute;
+	unsigned short day;
+	unsigned short month;
 	unsigned short year;
 
 public:
-	DateTime(unsigned char hour, unsigned char minute, unsigned char day, unsigned char month, unsigned short year);
+	DateTime(unsigned short hour, unsigned short minute, unsigned short day, unsigned short month, unsigned short year);
 	int compare(const DateTime& other);
+
+	unsigned short get_hour();
+	unsigned short set_hour();
+	unsigned short get_minute();
+	unsigned short set_mintue();
+	unsigned short get_day();
+	unsigned short set_day();
+	unsigned short get_month();
+	unsigned short set_month();
+	unsigned short get_year();
+	unsigned short set_year();
 };
 
 #endif
