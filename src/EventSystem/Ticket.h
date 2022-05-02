dfa8e48078a1ee90e2dbde5fbb2aba7b84de2897
Syndamia <kamen.d.mladenov@protonmail.com>
2022-05-02 07:55:09 +0300

    Autocommit at 02.05.2022 @ 07:55

--- src/EventSystem/Ticket.h
+++ src/EventSystem/Ticket.h
@@ -1,4 +1,15 @@
 #ifndef HEADER_EVENTSYSTEM_TICKET
 #define HEADER_EVENTSYSTEM_TICKET
 
+class Ticket {
+	unsigned row;
+	unsigned seat;
+
+public:
+	Ticket(unsigned row, unsigned seat);
+
+	unsigned get_row() const;
+	unsigned get_seat() const;
+};
+
 #endif
