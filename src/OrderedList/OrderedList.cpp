fd55aa8776c2ef1006d14d2d5f22177b3b0eb4e7
Syndamia <kamen.d.mladenov@protonmail.com>
2022-05-02 21:20:30 +0300

    Autocommit at 02.05.2022 @ 21:20

--- src/OrderedList/OrderedList.cpp
+++ src/OrderedList/OrderedList.cpp
@@ -1,12 +1,12 @@
 #include "OrderedList.h"
 
-bool OrderedList::insert(const T& element) {
-	if (length == count) resize();
+template <class T>
+bool OrderedList<T>::insert(const T& element) {
+	if (List<T>::length == List<T>::count) List<T>::resize();
 
-	unsigned l = 0, r = count - 1, m = l / 2 + r / 2;
-	while (l < r) {
-		if (elements[m].compare(element) > 0) r = m;
-		else if (elements[m].compare(element) < 0) l = m;
-		m = l / 2 + r / 2;
-	}
+	unsigned insertionInd = 0;
+	while (List<T>::elements[insertionInd].compare(element) < 0 && insertionInd < List<T>::count)
+		insertionInd++;
+
+	return List<T>::insertAt(element, insertionInd);
 }
