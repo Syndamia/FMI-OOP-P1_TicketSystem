2897ca7799a4377aa4a69579abfdd0580b59ccb6
Syndamia <kamen.d.mladenov@protonmail.com>
2022-05-02 07:45:05 +0300

    Autocommit at 02.05.2022 @ 07:45

--- src/EventSystem/Event.h
+++ src/EventSystem/Event.h
@@ -1,8 +1,11 @@
 #ifndef HEADER_EVENTSYSTEM_EVENT
 #define HEADER_EVENTSYSTEM_EVENT
 
+#include "../List/List.h"
+
 class Event {
-	
+	List<Ticket> tickets;
+	List<Reservation> reservations;
 };
 
 #endif
