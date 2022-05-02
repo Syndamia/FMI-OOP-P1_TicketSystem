27e25fa55f21842d819072bc5650a61088a5511a
Syndamia <kamen.d.mladenov@protonmail.com>
2022-05-02 20:20:10 +0300

    Autocommit at 02.05.2022 @ 20:20

--- src/EventSystem/Event.h
+++ src/EventSystem/Event.h
@@ -4,12 +4,25 @@
 #include "../List/List.h"
 #include "../DateTime/DateTime.h"
 #include "../String/String.h"
+#include "../Venue/Hall.h"
 #include "Ticket.h"
 #include "Reservation.h"
+#include "StatusCodes.h"
 
 class Event {
 	List<Ticket> tickets;
 	List<Reservation> reservations;
+	DateTime time;
+	String name;
+	Hall* hall;
+
+public:
+	Event();
+	Event(const Hall* hall, String name, DateTime time);
+
+	StatusCode reserveTicket();
+	StatusCode cancelReservation();
+	StatusCode buyReservation();
 };
 
 #endif
